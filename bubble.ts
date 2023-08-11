33307f8a15e62f6e4a6820204e673bc127adf3d1
balogun14 <awwalbalogun06@gmail.com>
2023-08-11 19:39:31 +0100

    turned it to a function

--- bubble.ts
+++ bubble.ts
@@ -1,4 +1,4 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+function bs_list(haystack: number[], needle: number): boolean {
     let lo = 0;
     let hi = haystack.length;
     do {
