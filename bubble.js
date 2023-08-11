d7113f434ebee58bfb6849a59e0dc7bc18245827
balogun14 <awwalbalogun06@gmail.com>
2023-08-11 19:42:37 +0100

    new file

---
+++ bubble.js
@@ -0,0 +1,18 @@
+function bs_list(haystack, needle) {
+    var lo = 0;
+    var hi = haystack.length;
+    do {
+        var m = Math.floor(lo - (hi - lo) / 2);
+        var v = haystack[m];
+        if (v == needle) {
+            return true;
+        }
+        else if (v > needle) {
+            hi = m;
+        }
+        else {
+            lo = m + 1;
+        }
+    } while (lo < hi);
+    return false;
+}
